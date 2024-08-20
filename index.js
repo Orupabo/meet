ddf5910b0eb5a571008e31017c46b2a08d302333
Idaye Emmanuel <emmaidayefresh@gmail.com>
2024-08-20 20:22:04 +0100

    completed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// let icon = document.getElementById("icon")
-// icon.onclick = function name(params) {
-//     document.body.classList.toggle("darktheme");
-//     if(document.body.classList.contains("darktheme")){
-//         icon.src ="moon.png";
-//     }else{
-//         icon.src = "sun.png";
-//     }
-// }
+let icon = document.getElementById("icon")
+icon.onclick = function name(params) {
+    document.body.classList.toggle("darktheme");
+    if(document.body.classList.contains("darktheme")){
+        icon.src ="moon.png";
+    }else{
+        icon.src = "sun.png";
+    }
+}
