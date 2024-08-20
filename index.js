bf34f776908fadbab455376fb6bbe8ec196231ba
Idaye Emmanuel <emmaidayefresh@gmail.com>
2024-08-20 19:09:15 +0100

    finished html and css and javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+let icon = document.getElementById("icon")
+icon.onclick = function name(params) {
+    document.body.classList.toggle("darktheme");
+    if(document.body.classList.contains("darktheme")){
+        icon.src ="moon.png";
+    }else{
+        icon.src = "sun.png";
+    }
+}
